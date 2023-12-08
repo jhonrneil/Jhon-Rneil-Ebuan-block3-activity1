29d006ea093d03dda22312ceeda7b50f701d93f9
Sofia <jhca.ebuan.up@phinmaed.com>
2023-12-08 16:58:06 +0800

    update1

--- app/src/main/java/com/ebuan/jhonrneil/block3/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/ebuan/jhonrneil/block3/p1/quiz/MainActivity.kt
@@ -2,10 +2,24 @@ package com.ebuan.jhonrneil.block3.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstNumber: EditText = findViewById(R.id.et_first_number)
+        val secondNumber: EditText = findViewById(R.id.et_second_number)
+        val thirdNumber: EditText = findViewById(R.id.et_third_number)
+        val fourthNumber: EditText = findViewById(R.id.et_fourth_number)
+        val fifthNumber: EditText = findViewById(R.id.et_fifth_number)
+        val btCompute: Button = findViewById(R.id.bt_compute)
+        val txNumber: TextView = findViewById(R.id.tx_number)
+        val btMinues: Button = findViewById(R.id.bt_minus)
+        val btTimes: Button = findViewById(R.id.bt_times)
+        val btDivide: Button = findViewById(R.id.bt_divide)
     }
 }
